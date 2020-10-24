cbab147de7ac7fcba88b1adb005c993495c7643d
Almudena <almudenablanco76@gmail.com>
2020-10-24 22:57:31 +0200

    Working on favourite card selection

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,16 @@
 console.log("Hola");
 
 const btnSearch = document.querySelector(".js-btn");
-
 let section2 = document.querySelector(".js-section2");
 
 //array de las series que me va a mostrar, que coge del fetch
 let dataSerials = [];
+let favoritesList = [];
 
 //función escuchadora del click-al hacer click recoje los datos que ha traido del servidor
 //function listener of click - get data from server when clicking
 function searchSerial() {
   const input = document.querySelector(".js-input").value; //value entered in input
-  console.log(input);
 
   fetch(`//api.tvmaze.com/search/shows?q=${input}`) // ahora solo tengo las que continen palabra: girls
     .then((response) => {
@@ -25,6 +24,7 @@ function searchSerial() {
       console.log(dataSerials);
       // paintTitle();
       paintCard();
+      listenFav();
     });
 }
 let newUl = "";
@@ -48,11 +48,9 @@ function paintCard() {
     const newImg = document.createElement("img"); //creo el <img>
     const nullImg = newImg.src;
     if (dataSerials[i].show.image != null) {
-      console.log("entró en if");
       newImg.src = dataSerials[i].show.image.medium; //añado a html la src de img: camino de la img
       newImg.alt = "Cartel de tu serie favorita"; //añado alt a img
     } else {
-      console.log("entró en else");
       newImg.src = `https://via.placeholder.com/210x295/ffffff/008000/?text=${title}`; //paint default img
       newImg.alt = "Imagen no disponible"; //añado alt a img
     }
@@ -64,6 +62,11 @@ function paintCard() {
     newDiv.appendChild(newImg); //meto <img> en <div>
 
     newLi = document.createElement("li"); //creo el <li>
+
+    newLi.setAttribute("class", "js-cardsItem"); // add class to <li>
+    newLi.setAttribute("id", `${i}`); //add id to <li>
+    // newLi.setAttribute("class", "container__containerCards--cardsItem");
+
     newLi.appendChild(newParagraph); //add <p> with title to <li>
     newLi.appendChild(newDiv); //meto <div> en <li>
 
@@ -71,5 +74,62 @@ function paintCard() {
   }
 }
 
+//listen event click
+function selectFav(ev) {
+  const indexClicked = parseInt(ev.currentTarget.id); //clicked ev
+  // favoritesList.push(clicked); //add new clicked ev to []
+  console.log(favoritesList);
+  console.log(indexClicked);
+
+  const elementsList = document.querySelectorAll("li");
+
+  for (const elementList of elementsList) {
+    if (elementList.id === indexClicked) {
+    }
+  }
+
+  //.find() => localiza el elem: retorna undefined si no se encuentra, y si no su valor
+
+  const theFavorite = favoritesList.find(
+    (favoriteList) => favoriteList.id === indexClicked
+  );
+  console.log(theFavorite);
+
+  if (theFavorite === undefined) {
+    //buscar
+    const theFavId = dataSerials.findIndex(
+      (dataSerial) => dataSerial.show.id === indexClicked
+    );
+
+    favoritesList.push(theFavId);
+
+    console.log("entra en if");
+    console.log(favoritesList);
+  } else {
+    // favoritesList
+    console.log("entra en else");
+  }
+
+  // for (const favoriteList of favoritesList) {
+  //   if (favoriteList === clicked) {
+  //     favoriteList.add("container__containerCards--cardsItem");
+  //     console.log("entra en if");
+  //   } else {
+  //     console.log("entra en else");
+
+  // favoritesList.remove("container__containerCards--cardsItem");
+  //   }
+  // }
+  // favoritesList.find;
+}
+
+//event click to select fav
+function listenFav() {
+  const cardsItems = document.querySelectorAll(".js-cardsItem"); //no puedo subirlo arriba porque aún no existe la class
+  for (const cardsItem of cardsItems) {
+    cardsItem.addEventListener("click", selectFav);
+  }
+}
+
 btnSearch.addEventListener("click", searchSerial);
-btnSearch.click(); // para quitar al final--se clicka el bton al cargar pagina
+btnSearch.click(); // para quitar al final--se clicka el bton al cargar página
