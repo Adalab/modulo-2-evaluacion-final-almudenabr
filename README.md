c65c17d62b048d0b4d8d7503df93d2359acf4152
Almudena <almudenablanco76@gmail.com>
2020-10-24 13:00:49 +0200

    Show default img when serial does not have one

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 1.3-[x] Pintar una foto
 1.3.1- [x] pintar título de la serie en el html
 1.4-[x] pintar todas las series que contengan la palabra introducida
-1.5-[] Si no hay foto mostrar img de relleno
-1.6-[] Al hacer nueva búsqueda borrar la búsqueda anterior
+1.5-[x] Si no hay foto mostrar img de relleno
+1.6-[x] Al hacer nueva búsqueda borrar la búsqueda anterior
 
 2-[] MARCAR COMO FAVORITAS LAS SELECCIONADAS POR LA USUARIA
 
--- src/js/main.js
+++ src/js/main.js
@@ -38,7 +38,7 @@ function paintCard() {
   const ul = document.querySelector(".js-containerCards");
 
   if (ul != null) {
-    ul.innerHTML = "";
+    ul.innerHTML = ""; //vaciar <ul> html
   }
 
   for (let i = 0; i < dataSerials.length; i++) {
@@ -46,8 +46,16 @@ function paintCard() {
     newTitle = document.createTextNode(`${title}`); //create title of serial to be printed
 
     const newImg = document.createElement("img"); //creo el <img>
-    newImg.src = dataSerials[i].show.image.medium; //añado a html la src de img: camino de la img
-    newImg.alt = "Cartel de tu serie favorita"; //añado alt a img
+    const nullImg = newImg.src;
+    if (dataSerials[i].show.image != null) {
+      console.log("entró en if");
+      newImg.src = dataSerials[i].show.image.medium; //añado a html la src de img: camino de la img
+      newImg.alt = "Cartel de tu serie favorita"; //añado alt a img
+    } else {
+      console.log("entró en else");
+      newImg.src = `https://via.placeholder.com/210x295/ffffff/008000/?text=${title}`; //paint default img
+      newImg.alt = "Imagen no disponible"; //añado alt a img
+    }
 
     newParagraph = document.createElement("p"); //creo <p>
     newParagraph.appendChild(newTitle); //add title to <p>
